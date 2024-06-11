2f2e5599018759056dcb60d57ba3ab6becbbefa3
LesterCantor <c012718j@student.staffs.ac.uk>
2024-06-11 20:38:08 +0100

    replicated sprinting with custom cmc

---
+++ Source/Project_Games_Day/Public/TestCharacter.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TestCharacter.generated.h"
+
+UCLASS(config=Game)
+class PROJECT_GAMES_DAY_API ATestCharacter : public ACharacter
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Movement) class UDezCharacterMovementComponent* DezCharacterMovementComponent;
+
+public:
+	// Sets default values for this character's properties
+	ATestCharacter(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
