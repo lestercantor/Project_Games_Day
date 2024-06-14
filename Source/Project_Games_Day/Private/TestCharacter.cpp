3e995d51eb7105544aa9d73a3aac2c97e8700147
LesterCantor <c012718j@student.staffs.ac.uk>
2024-06-14 22:08:03 +0100

    different crouch implementation

--- Source/Project_Games_Day/Private/TestCharacter.cpp
+++ Source/Project_Games_Day/Private/TestCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "TestCharacter.h"
 #include "DezCharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ATestCharacter::ATestCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.SetDefaultSubobjectClass<UDezCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
@@ -12,13 +13,55 @@ ATestCharacter::ATestCharacter(const FObjectInitializer& ObjectInitializer) : Su
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	PlayerCamera->SetupAttachment(GetRootComponent());
+	PlayerCamera->SetRelativeLocation(FVector(-10.0f, 0.0f, 60.0f));
+	PlayerCamera->bUsePawnControlRotation = true;
+
+	CrouchEyeOffset = FVector::ZeroVector;
+	CrouchSpeed = 12.0f;
+}
+
+void ATestCharacter::OnStartCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust)
+{
+	if (HalfHeightAdjust == 0)
+	{
+		return;
+	}
+
+	float StartBaseEyeHeight = BaseEyeHeight;
+	Super::OnStartCrouch(HalfHeightAdjust, ScaledHalfHeightAdjust);
+	CrouchEyeOffset.Z += StartBaseEyeHeight - BaseEyeHeight + HalfHeightAdjust;
+	PlayerCamera->SetRelativeLocation(FVector(0.0f, 0.0f, BaseEyeHeight), false);
+}
+
+void ATestCharacter::OnEndCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust)
+{
+	if (HalfHeightAdjust == 0)
+	{
+		return;
+	}
+
+	float StartBaseEyeHeight = BaseEyeHeight;
+	Super::OnStartCrouch(HalfHeightAdjust, ScaledHalfHeightAdjust);
+	CrouchEyeOffset.Z += StartBaseEyeHeight - BaseEyeHeight - HalfHeightAdjust;
+	PlayerCamera->SetRelativeLocation(FVector(0.0f, 0.0f, BaseEyeHeight), false);
+}
+
+void ATestCharacter::CalcCamera(float DeltaTime, FMinimalViewInfo& OutResult)
+{
+	if (PlayerCamera)
+	{
+		PlayerCamera->GetCameraView(DeltaTime, OutResult);
+		OutResult.Location += CrouchEyeOffset;
+	}
 }
 
 // Called when the game starts or when spawned
 void ATestCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -26,6 +69,8 @@ void ATestCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	float CrouchInterpTime = FMath::Min(1.0f, CrouchSpeed * DeltaTime);
+	CrouchEyeOffset = (1.0f - CrouchInterpTime) * CrouchEyeOffset;
 }
 
 // Called to bind functionality to input
