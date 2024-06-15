e6715144bba1d5bb746ce408bddc1b5e5e43a67a
LesterCantor <c012718j@student.staffs.ac.uk>
2024-06-15 15:18:20 +0100

    smooth crouching with children mesh following instead of snapping

--- Source/Project_Games_Day/Private/DezPlayerCameraManager.cpp
+++ Source/Project_Games_Day/Private/DezPlayerCameraManager.cpp
@@ -11,36 +11,35 @@ ADezPlayerCameraManager::ADezPlayerCameraManager()
 {
 }
 
-void ADezPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
-{
-	Super::UpdateViewTarget(OutVT, DeltaTime);
-
-	/*if (ATestCharacter* TestCharacter = Cast<ATestCharacter>(GetOwningPlayerController()->GetPawn()))
-	{
-		UDezCharacterMovementComponent* DezCMC = TestCharacter->GetDezCharacterMovement();
-		FVector TargetCrouchOffset = FVector(
-			0,
-			0, 
-			DezCMC->GetCrouchedHalfHeight() - TestCharacter->GetClass()->GetDefaultObject<ACharacter>()->GetCapsuleComponent()->GetScaledCapsuleHalfHeight());
-
-		FVector Offset = FMath::Lerp(FVector::ZeroVector, TargetCrouchOffset, FMath::Clamp(CrouchBlendTime / CrouchBlendDuration, 0.0f, 1.0f));
-		
-
-		if (DezCMC->IsCrouching())
-		{
-			CrouchBlendTime = FMath::Clamp(CrouchBlendTime + DeltaTime, 0.0f, CrouchBlendDuration);
-			Offset -= TargetCrouchOffset;
-		}
-		else
-		{
-			CrouchBlendTime = FMath::Clamp(CrouchBlendTime - DeltaTime, 0.0f, CrouchBlendDuration);
-
-		}
-
-		if (DezCMC->IsMovingOnGround())
-		{
-			OutVT.POV.Location += Offset;
-		}
-		
-	}*/
-}
+//void ADezPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
+//{
+//	Super::UpdateViewTarget(OutVT, DeltaTime);
+//
+//	if (ATestCharacter* TestCharacter = Cast<ATestCharacter>(GetOwningPlayerController()->GetPawn()))
+//	{
+//		UDezCharacterMovementComponent* DezCMC = TestCharacter->GetDezCharacterMovement();
+//		FVector TargetCrouchOffset = FVector(
+//			0,
+//			0, 
+//			DezCMC->GetCrouchedHalfHeight() - TestCharacter->GetClass()->GetDefaultObject<ACharacter>()->GetCapsuleComponent()->GetScaledCapsuleHalfHeight());
+//
+//		FVector Offset = FMath::Lerp(FVector::ZeroVector, TargetCrouchOffset, FMath::Clamp(CrouchBlendTime / CrouchBlendDuration, 0.0f, 1.0f));
+//		
+//
+//		if (DezCMC->IsCrouching())
+//		{
+//			CrouchBlendTime = FMath::Clamp(CrouchBlendTime + DeltaTime, 0.0f, CrouchBlendDuration);
+//			Offset -= TargetCrouchOffset;
+//		}
+//		else
+//		{
+//			CrouchBlendTime = FMath::Clamp(CrouchBlendTime - DeltaTime, 0.0f, CrouchBlendDuration);
+//
+//		}
+//
+//		if (DezCMC->IsMovingOnGround())
+//		{
+//			OutVT.POV.Location += Offset;
+//		}
+//	}
+//}
